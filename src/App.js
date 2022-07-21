10972fc6315204c939780b29ffc47c72058e29a3
addTvb <arslan.web.dev@gmail.com>
2022-07-21 08:17:04 +0500

    upgrade version

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
+export default function App() {
+	// JS
+	const [tasks, setTasks] = useState(['Learn React.js', 'Complete to-do list']);
 	const [input, setInput] = useState('');
-	const [tasks, setTasks] = useState(['Learn React.js', 'Perfectly pass exam']);
 
-	const handleInput = (event) => setInput(event.target.value);
-	const handleSubmit = () => setTasks([...tasks, input]);
-	const handleDelete = (task) => {
-		const filteredArr = tasks.filter((item) => item !== task);
-		setTasks(filteredArr);
+	const handleChangeInput = (event) => {
+		setInput(event.target.value);
+	};
+
+	const handleSubmit = () => {
+		if (input !== '') {
+			setTasks([...tasks, input]);
+			setInput('');
+		} else {
+			alert('Введите что нибудь!');
+		}
+	};
+
+	const handleDelete = (taskIndex) => {
+		const filteredArray = tasks.filter((task, index) => {
+			return index !== taskIndex;
+		});
+
+		setTasks(filteredArray);
 	};
 
 	return (
+		// JSX
 		<div>
 			<div>
-				<input
-					value={input}
-					onChange={handleInput}
-					className='border border-black'
-				/>
-				<button
-					className='py-1 px-3 bg-blue-400 rounded-md transition-all border-2 border-black active:scale-95'
-					onClick={handleSubmit}
-				>
-					Add task
-				</button>
+				<input value={input} onChange={handleChangeInput} />
+				<button onClick={handleSubmit}>Add task</button>
 			</div>
-			<ul>
-				{tasks.map((task, index) => (
-					<li key={index} className='mb-1'>
-						{index + 1}. {task}
-						<button
-							onClick={() => handleDelete(task)}
-							className='p-1 bg-red-500'
-						>
-							X
-						</button>
-					</li>
-				))}
-			</ul>
+			<div>
+				<ol>
+					{tasks.map(function (task, taskIndex) {
+						return (
+							<li key={taskIndex}>
+								{task}
+								<button onClick={() => handleDelete(taskIndex)}>X</button>
+							</li>
+						);
+					})}
+				</ol>
+			</div>
 		</div>
 	);
 }
-
-export default App;

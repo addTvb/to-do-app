c9d19165ad0f1039b9a6aec65913d16bef3d05a0
addTvb <arslan.web.dev@gmail.com>
2022-09-03 12:03:34 +0500

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -45,22 +45,43 @@ export default function App() {
 
 	return (
 		// JSX
-		<div>
-			<div>
-				<input value={input} onChange={handleChangeInput} />
-				<button onClick={handleSubmit}>Add task</button>
-			</div>
-			<div>
-				<ol>
-					{tasks.map(function (task, taskIndex) {
-						return (
-							<li key={taskIndex}>
-								{task}
-								<button onClick={() => handleDelete(taskIndex)}>X</button>
-							</li>
-						);
-					})}
-				</ol>
+		<div className='w-screen flex justify-center items-center h-screen'>
+			<div className='w-[400px] flex flex-col gap-y-1'>
+				<div className='flex '>
+					<input
+						value={input}
+						onChange={handleChangeInput}
+						className='border border-black p-2 w-full'
+					/>
+					<button
+						onClick={handleSubmit}
+						className='border font-medium border-black px-4 py-2 hover:bg-black hover:text-white transition-all min-w-[100px]'
+					>
+						Add task
+					</button>
+				</div>
+				<div>
+					<ol className='flex gap-y-2 flex-col text-lg'>
+						{tasks.map(function (task, taskIndex) {
+							return (
+								<li
+									key={taskIndex}
+									className='flex gap-x-2 items-center justify-between'
+								>
+									<span>
+										<b>{taskIndex + 1}</b>) {task}
+									</span>
+									<button
+										onClick={() => handleDelete(taskIndex)}
+										className='px-2 font-medium border-2 border-red-500 text-xl hover:bg-red-500 hover:text-white transition-all'
+									>
+										X
+									</button>
+								</li>
+							);
+						})}
+					</ol>
+				</div>
 			</div>
 		</div>
 	);
